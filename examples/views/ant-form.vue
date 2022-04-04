0766cdc22b84fb0cb6a92ccffbe358b7e3ab0a26
mazca <mazca@yonyou.com>
2022-04-04 15:54:31 +0800

    关于AInput输入值的丢失疑问；render渲染时机的疑问

---
+++ examples/views/ant-form.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <InputDemo></InputDemo>
+
+    <a-form :form="form" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }">
+      <a-form-item>
+        <a-input v-decorator="['field1', { initialValue: 'abc' }]"></a-input>
+      </a-form-item>
+    </a-form>
+  </div>
+</template>
+<script>
+import InputDemo from './input-demo';
+export default {
+  components: { InputDemo },
+  data() {
+    return {
+      form: this.$form.createForm(this),
+    };
+  },
+};
+</script>
---
+++ examples/views/input-demo.vue
@@ -0,0 +1,26 @@
+<script>
+/**
+ * 为什么antv的AInput没有用v-model或者没有在onChange事件中改变value, blur时输入的值会自动重置？
+ * 因为其内部监听了input的blur事件，在blur中做了forceUpdate
+ *
+ */
+window.inputValue = '123';
+export default {
+  created() {
+    this.a = '456';
+  },
+  render() {
+    console.log('render');
+    return <a-input value={this.a} onChange={this.changeHandler}></a-input>;
+    // return <input type='text' value={window.inputValue} onChange={this.changeHandler}/>
+  },
+  methods: {
+    changeHandler(e) {
+      console.log(e.target.value);
+      window.inputValue = e.target.value;
+      this.a = e.target.value;
+      // this.$forceUpdate()
+    },
+  },
+};
+</script>
